60fca077ef257003b552e6e5b860910c7c16b419
Joseph Pecoraro <joepeck02@gmail.com>
2009-05-24 22:07:31 -0400

    Added the missing require

--- templates/ruby_options.rb
+++ templates/ruby_options.rb
@@ -1,14 +1,11 @@
 __FILE:ruby.rb__
 # Parse potential command line arguments
+require 'optparse'
 program_name = $0.split(/\//).last
-options = { :cat => false }
+options = { :cat => false, :output => nil }
 OptionParser.new do |opts|
 	opts.banner = "usage: #{program_name} [option] [files]"
-	opts.on("-c", "--cat", "Print the output to the shell.") do |u|
-		options[:cat] = true
-	end
-	opts.on_tail("-h", "--help", "Show this message") do
-		puts opts
-		exit
-	end
+	opts.on("-o FILE", "--output FILE", "Write directly to a file") { |f| options[:output] = f }
+	opts.on("-c", "--cat", "Print the output to the shell.") { |u| options[:cat] = true }
+	opts.on_tail("-h", "--help", "Show this message") { puts opts; exit }
 end.parse!
